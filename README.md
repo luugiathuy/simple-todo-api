e202146f00aa1f168af0e6ded1b551d0091e1069
Luu Gia Thuy <luugiathuy@gmail.com>
2015-11-12 08:36:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Todo JSON API
+
+Tutorial from [The New Stack](http://thenewstack.io/make-a-restful-json-api-go/) article.
---
+++ handlers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
+}
