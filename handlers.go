5f364872b5d6bf0752ebb24f984dc3fcbe2856f4
Luu Gia Thuy <luugiathuy@gmail.com>
2015-11-12 23:01:21 +0800

    Add jsonErr struct. Implement TodoShow handler

--- handlers.go
+++ handlers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -24,8 +25,27 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoId)
+	var todoId int
+	var err error
+	if todoId, err = strconv.Atoi(vars["todoId"]); err != nil {
+		panic(err)
+	}
+	todo := RepoFindTodo(todoId)
+	if todo.Id > 0 {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(todo); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	// If we did not find it, 404
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusNotFound)
+	if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusNotFound, Text: "Not Found"}); err != nil {
+		panic(err)
+	}
 }
 
 func TodoCreate(w http.ResponseWriter, r *http.Request) {
